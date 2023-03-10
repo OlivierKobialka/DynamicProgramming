605610f898c3d8663197ff8c0dd3e68f2dc3714a
OlivierKobialka <113932706+OlivierKobialka@users.noreply.github.com>
2023-03-10 10:15:52 +0100

    Create insertSort.ts

---
+++ Sorting/Insert Sort/insertSort.ts
@@ -0,0 +1,17 @@
+let InsertionSort = (array: number[]) => {
+	let i: number = 1;
+	let sortedArray: number[] = array;
+	while (i < sortedArray.length) {
+		let j: number = i;
+		while (j > 0 && sortedArray[j--] < sortedArray[j]) {
+			[sortedArray[j], sortedArray[j + 1]] = [
+				sortedArray[j + 1],
+				sortedArray[j],
+			];
+		}
+		i++;
+	}
+	console.log(`Sorted Arr: ${sortedArray}`);
+};
+
+InsertionSort([32, 1, 42, 12, 64, 23, 6543, 23]);
