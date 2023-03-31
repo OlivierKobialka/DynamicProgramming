62dc78406cae1d6ea56e58e464b46879e3e7386a
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-03-31 09:03:10 +0200

    starter

---
+++ words/intToWord.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+const intToWord = num => {
+	const words = [
+		"Zero",
+		"One",
+		"Two",
+		"Three",
+		"Four",
+		"Five",
+		"Six",
+		"Seven",
+		"Eight",
+		"Nine",
+		"Ten",
+		"Eleven",
+		"Twelve",
+		"Thirteen",
+		"Fourteen",
+		"Fifteen",
+		"Sixteen",
+		"Seventeen",
+		"Eighteen",
+		"Nineteen",
+		"Twenty",
+		"Thirty",
+		"Forty",
+		"Fifty",
+		"Sixty",
+		"Seventy",
+		"Eighty",
+		"Ninety",
+		"Hundred",
+		"Thousand",
+		"Million",
+	];
+
+	const int = [
+		1000000, 1000, 100, 90, 80, 70, 60, 50, 40, 30, 20, 19, 18, 17, 16, 15, 14,
+		13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+	];
+
+	let str = "";
+
+	for (let i=0; i<int.length; i++){
+		if 
+	}
+};
+
+intToWord(1);
