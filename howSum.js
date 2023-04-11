79fa5ba02ea8460a5c89c466543e8ef12d078ea2
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-11 15:59:00 +0200

    finish

--- howSum.js
+++ howSum.js
@@ -1,6 +1,13 @@
 const howSum = (targetSum, numbers) => {
 	if (targetSum === 0) return [];
 	if (targetSum < 0) return null;
+
+	for (let num of numbers) {
+		const remainder = targetSum - num;
+		const remainderResult = howSum(remainder, numbers);
+		if (remainderResult !== null) return [...remainderResult, num];
+	}
+    return null
 };
 
 console.log(howSum(7, [2, 3]));
