fee7a3246f100f6198bd1f83e4447a45df31561b
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-04-11 15:42:58 +0200

    memo

--- canSum.js
+++ canSum.js
@@ -1,13 +1,25 @@
-const canSum = (targetSum, numbers) => {
+const canSum = (targetSum, numbers, memo = {}) => {
+	if (targetSum in memo) return memo[targetSum];
 	if (targetSum === 0) return true;
 	if (targetSum < 0) return false;
 
 	for (let num of numbers) {
 		const remainder = targetSum - num;
-		if (canSum(remainder, numbers) === true) return true;
+		if (canSum(remainder, numbers, memo) === true) return true;
 	}
+	memo[targetSum] = false;
 	return false;
 };
+// const canSum = (targetSum, numbers) => {
+// 	if (targetSum === 0) return true;
+// 	if (targetSum < 0) return false;
+
+// 	for (let num of numbers) {
+// 		const remainder = targetSum - num;
+// 		if (canSum(remainder, numbers) === true) return true;
+// 	}
+// 	return false;
+// };
 
 console.log(canSum(7, [2, 3]));
 console.log(canSum(7, [5, 3, 4, 7]));
