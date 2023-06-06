b5579a602b1bbb629c29dbb965316a15569c0afe
_kvbialka917 <113932706+OlivierK07@users.noreply.github.com>
2023-06-06 23:22:46 +0200

    fix w/ rl

--- words/intToWord.js
+++ words/intToWord.js
@@ -1,52 +1,104 @@
-/**
- * @param {number} num
- * @return {string}
- */
-const intToWord = num => {
-	const words = [
-		"Zero",
-		"One",
-		"Two",
-		"Three",
-		"Four",
-		"Five",
-		"Six",
-		"Seven",
-		"Eight",
-		"Nine",
-		"Ten",
-		"Eleven",
-		"Twelve",
-		"Thirteen",
-		"Fourteen",
-		"Fifteen",
-		"Sixteen",
-		"Seventeen",
-		"Eighteen",
-		"Nineteen",
-		"Twenty",
-		"Thirty",
-		"Forty",
-		"Fifty",
-		"Sixty",
-		"Seventy",
-		"Eighty",
-		"Ninety",
-		"Hundred",
-		"Thousand",
-		"Million",
+const readline = require("readline");
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
+});
+
+function numberToWord(number) {
+	// Define word arrays for numbers up to 19 and for tens
+	const units = [
+		"zero",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+		"ten",
+		"eleven",
+		"twelve",
+		"thirteen",
+		"fourteen",
+		"fifteen",
+		"sixteen",
+		"seventeen",
+		"eighteen",
+		"nineteen",
 	];
 
-	const int = [
-		1000000, 1000, 100, 90, 80, 70, 60, 50, 40, 30, 20, 19, 18, 17, 16, 15, 14,
-		13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0,
+	const tens = [
+		"",
+		"",
+		"twenty",
+		"thirty",
+		"forty",
+		"fifty",
+		"sixty",
+		"seventy",
+		"eighty",
+		"ninety",
 	];
 
-	let str = "";
-
-	for (let i=0; i<int.length; i++){
-		if 
+	// Function to convert a two-digit number to its word representation
+	function convertTwoDigit(number) {
+		if (number < 20) {
+			return units[number];
+		} else {
+			const unit = number % 10;
+			const ten = Math.floor(number / 10);
+			return tens[ten] + (unit !== 0 ? "-" + units[unit] : "");
+		}
 	}
-};
 
-intToWord(1);
+	// Handle special cases for zero and negative numbers
+	if (number === 0) {
+		return "zero";
+	} else if (number < 0) {
+		return "minus " + numberToWord(-number);
+	}
+
+	// Convert the number to its word representation
+	let word = "";
+
+	// Handle billions
+	if (number >= 1000000000) {
+		word += numberToWord(Math.floor(number / 1000000000)) + " billion ";
+		number %= 1000000000;
+	}
+
+	// Handle millions
+	if (number >= 1000000) {
+		word += numberToWord(Math.floor(number / 1000000)) + " million ";
+		number %= 1000000;
+	}
+
+	// Handle thousands
+	if (number >= 1000) {
+		word += numberToWord(Math.floor(number / 1000)) + " thousand ";
+		number %= 1000;
+	}
+
+	// Handle hundreds
+	if (number >= 100) {
+		word += numberToWord(Math.floor(number / 100)) + " hundred ";
+		number %= 100;
+	}
+
+	// Handle tens and units
+	if (number > 0) {
+		word += convertTwoDigit(number);
+	}
+
+	return word.trim();
+}
+
+rl.question("Enter an integer: ", input => {
+	const number = parseInt(input);
+	const word = numberToWord(number);
+	console.log(word);
+	rl.close();
+});
